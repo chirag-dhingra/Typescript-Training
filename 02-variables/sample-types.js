282eb2879cc9b7d6191b1773cf197f13ade42502
dhingra <chirag.dhingra@adesso.de>
2023-08-31 14:10:28 +0200

    defining variables

---
+++ 02-variables/sample-types.js
@@ -0,0 +1,7 @@
+var found = true;
+var grade = 2.5;
+var firstName = "Milkha";
+var lastName = "Singh";
+console.log(found);
+console.log("The grade is: " + grade);
+console.log("Hi " + firstName + " " + lastName);
---
+++ 02-variables/sample-types.ts
@@ -0,0 +1,8 @@
+let found : boolean = true;
+let grade : number  = 2.5;
+let firstName: string = "Milkha"
+let lastName: string = "Singh"
+
+console.log(found);
+console.log("The grade is: "+ grade);
+console.log("Hi "+ firstName + " "+ lastName);
