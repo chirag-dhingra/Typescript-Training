ea2cd312eb4049bb6707202503e75b55fd929fb9
dhingra <chirag.dhingra@adesso.de>
2023-09-12 10:50:21 +0200

    modules

---
+++ 04-classes/04-modules/Driver.js
@@ -0,0 +1,6 @@
+"use strict";
+exports.__esModule = true;
+var Customer_1 = require("./Customer");
+var myCustomer = new Customer_1.Customer("Milkha", "Singh");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
---
+++ 04-classes/04-modules/Driver.ts
@@ -0,0 +1,5 @@
+import {Customer } from "./Customer"
+
+let myCustomer = new Customer("Milkha", "Singh");
+console.log(myCustomer.firstName);
+console.log(myCustomer.lastName);
